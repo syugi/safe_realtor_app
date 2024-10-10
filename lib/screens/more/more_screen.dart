a0635cc80897398139172987c7011e4cc82c9eed
syugi <altnr23@gmail.com>
2024-10-10 16:15:37 +0900

    refact: 로그인 안한경우 더보기 메뉴 수정

--- lib/screens/more/more_screen.dart
+++ lib/screens/more/more_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:safe_realtor_app/screens/auth/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:safe_realtor_app/screens/home.dart';
 import 'package:safe_realtor_app/constants/role_constants.dart';
 
-class MoreScreen extends StatelessWidget {
+class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
+
+  @override
+  _MoreScreenState createState() => _MoreScreenState();
+}
+
+class _MoreScreenState extends State<MoreScreen> {
+  bool _isLoggedIn = false;
+  Map<String, String>? _accountInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userId = prefs.getString('userId');
+    if (userId != null) {
+      setState(() {
+        _isLoggedIn = true;
+        _accountInfo = {
+          'userId': userId,
+          'role': prefs.getString('role') ?? 'ROLE_USER',
+        };
+      });
+    } else {
+      setState(() {
+        _isLoggedIn = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,91 +46,82 @@ class MoreScreen extends StatelessWidget {
         title: const Text('더보기'),
       ),
       body: ListView(
-        children: [
-          // 계정 정보 메뉴
-          FutureBuilder<Map<String, String>>(
-            future: _getAccountInfo(), // 계정 정보를 가져오는 비동기 함수 호출
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text('계정 정보'),
-                  subtitle: Text('로딩 중...'),
-                );
-              } else if (snapshot.hasError || !snapshot.hasData) {
-                return const ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text('계정 정보'),
-                  subtitle: Text('계정 정보를 불러올 수 없습니다.'),
-                );
-              } else {
-                // 계정 정보가 있을 경우 표시
-                final data = snapshot.data!;
-                final roleDescription =
-                    UserRoles.getRoleDescription(data['role']!);
-
-                return ListTile(
-                  leading: const Icon(Icons.account_circle),
-                  title: const Text('계정 정보'),
-                  subtitle: Text(
-                    'ID: ${data['userId']} ${roleDescription.isNotEmpty ? '($roleDescription)' : ''}',
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AccountScreen()),
-                    );
-                  },
-                );
-              }
-            },
-          ),
-
-          // // 설정 메뉴
-          // ListTile(
-          //   leading: const Icon(Icons.settings),
-          //   title: const Text('설정'),
-          //   onTap: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(builder: (context) => const SettingsScreen()),
-          //     );
-          //   },
-          // ),
-
-          // 로그아웃 메뉴
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('로그아웃'),
-            onTap: () {
-              _logout(context);
-            },
-          ),
-
-          // 앱 정보 메뉴 (버전 정보 바로 표시)
-          ListTile(
-            leading: const Icon(Icons.info_outline),
-            title: const Text('앱 정보'),
-            subtitle: const Text('버전 1.0.0'), // 여기서 버전 정보를 바로 표시
-            onTap: () {
-              _showAppInfo(context);
-            },
-          ),
-        ],
+        children: _isLoggedIn
+            ? _buildLoggedInMenu(context)
+            : _buildLoggedOutMenu(context),
       ),
     );
   }
 
-  // 사용자 ID와 역할(중개사 또는 관리자)을 가져오는 함수
-  Future<Map<String, String>> _getAccountInfo() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String userId = prefs.getString('userId') ?? '알 수 없음';
-    String role = prefs.getString('role') ?? 'ROLE_USER'; // 기본값을 ROLE_USER로 설정
-    return {'userId': userId, 'role': role};
+  // 로그인한 상태의 메뉴 항목들
+  List<Widget> _buildLoggedInMenu(BuildContext context) {
+    return [
+      _buildAccountInfoTile(),
+      _buildMenuTile(
+        icon: Icons.logout,
+        title: '로그아웃',
+        onTap: () => _logout(context),
+      ),
+      _buildAppInfoTile(context),
+    ];
   }
 
-  // 앱 정보 다이얼로그 표시 함수
+  // 로그인하지 않은 상태의 메뉴 항목들
+  List<Widget> _buildLoggedOutMenu(BuildContext context) {
+    return [
+      _buildMenuTile(
+        icon: Icons.login,
+        title: '로그인',
+        onTap: () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginScreen())),
+      ),
+      _buildAppInfoTile(context),
+    ];
+  }
+
+  // 계정 정보 표시
+  Widget _buildAccountInfoTile() {
+    final roleDescription =
+        UserRoles.getRoleDescription(_accountInfo?['role'] ?? '');
+    return ListTile(
+      leading: const Icon(Icons.account_circle),
+      title: const Text('계정 정보'),
+      subtitle: Text(
+          'ID: ${_accountInfo?['userId']} ${roleDescription.isNotEmpty ? '($roleDescription)' : ''}'),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AccountScreen()),
+        );
+      },
+    );
+  }
+
+  // 공통 메뉴 항목 생성 함수
+  Widget _buildMenuTile(
+      {required IconData icon,
+      required String title,
+      required VoidCallback onTap}) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: onTap,
+    );
+  }
+
+  // 앱 정보 메뉴
+  Widget _buildAppInfoTile(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.info_outline),
+      title: const Text('앱 정보'),
+      subtitle: const Text('버전 1.0.0'),
+      onTap: () {
+        _showAppInfo(context);
+      },
+    );
+  }
+
+  // 앱 정보 다이얼로그 표시
   void _showAppInfo(BuildContext context) {
     showDialog(
       context: context,
@@ -118,7 +142,7 @@ class MoreScreen extends StatelessWidget {
     );
   }
 
-  // 로그아웃 처리 함수
+  // 로그아웃 처리
   Future<void> _logout(BuildContext context) async {
     showDialog(
       context: context,
@@ -147,29 +171,17 @@ class MoreScreen extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const HomeScreen()),
                   (Route<dynamic> route) => false, // 이전 모든 라우트 제거
                 );
+
+                // 로그아웃 성공 알림
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('로그아웃 되었습니다.')),
+                );
               },
               child: const Text('확인'),
             ),
           ],
         );
       },
-    );
-  }
-}
-
-// 예시용 설정 페이지
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('설정'),
-      ),
-      body: const Center(
-        child: Text('설정 페이지 내용'),
-      ),
     );
   }
 }
